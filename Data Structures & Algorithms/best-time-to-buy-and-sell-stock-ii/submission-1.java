084c3a7a6621ef18b5cd15b0dc010561c9f3a3d7
Ria <166983351+riadhh-302005@users.noreply.github.com>
2026-05-21 22:56:30 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int sum=0;
+        for(int i=1;i<n;i++){
+            int val=prices[i]-prices[i-1];
+            if(val>0){
+                sum+=val;
+            }
+        }
+        return sum;
+    }
+}
