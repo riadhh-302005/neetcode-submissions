c42b00b886d583bbb0c902a361a5ebbba933eb8c
Ria <166983351+riadhh-302005@users.noreply.github.com>
2026-05-21 20:08:21 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int left=0;
+        int right=n-1;
+        int mid=0;
+        while(mid<=right){
+           if(nums[mid]==0){
+            int temp=nums[mid];
+            nums[mid]=nums[left];
+            nums[left]=temp;
+            left++;
+            mid++;
+           }
+           else if(nums[mid]==1){
+            mid++;
+           }
+           else{
+            int temp=nums[mid];
+            nums[mid]=nums[right];
+            nums[right]=temp;
+            right--;
+           }
+
+        }
+    }
+}
